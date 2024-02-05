df003c20a0bad3584b5b4199ce4fa493ad46edb4
karim announ <karim.announ@g.enp.edu.dz>
2024-02-05 21:24:32 +0100

    new

--- src/Pages/myCommandes/settings/components/EmailUpdate.tsx
+++ src/Pages/myCommandes/settings/components/EmailUpdate.tsx
@@ -10,6 +10,7 @@ import { setUserInfo } from '../../../../storeRedux/CartSlice';
 import { LoadingButton } from '@mui/lab';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import { useNavigate } from 'react-router-dom';
 export const EmailUpdate = () => {
 
   const [opens, setOpens] = useState(false);
@@ -26,14 +27,19 @@ export const EmailUpdate = () => {
 
   const [loading,setLoading]=useState(false)
   const [message,setMessage]=useState('')
+  const [newEmail,setNewEmail]=useState('')
+  const [show,setShow]=useState(false)
 
   const userInfo=useSelector((state:any)=>state.app.userInfo)
 
   const dispatch=useDispatch()
-    
+    const navigate=useNavigate()
 
       const [selectedCode, setSelectedCode] = useState(null);
-      const [sendedCode,setSendedCode]=useState('')
+   
+
+      const [seconds, setSeconds] = useState(60);
+
 
       const handelUpdateEmail=async()=>{
 
@@ -51,12 +57,26 @@ export const EmailUpdate = () => {
          
         });
         const data = await response.json()
-        console.log(data)
+    
         if (!data) {
           setLoading(true)
         }if (data.success == true) {
-        //  setSendedCode(data.confirmationCode)
           setLoading(false) 
+
+          const intervalId = setInterval(() => {
+            setSeconds((prevSeconds) => prevSeconds - 1);
+          }, 1000);
+        
+          // Stop the timer after 60 seconds
+          const stopTimerTimeout = setTimeout(() => {
+            clearInterval(intervalId);
+          }, 60000);
+        
+          // Clean up the interval and timeout when the component unmounts
+          return () => {
+            clearInterval(intervalId);
+            clearTimeout(stopTimerTimeout);
+          };
         }if (data.success == false) {
           setMessage(data.message)
           setOpens(true)
@@ -79,16 +99,18 @@ export const EmailUpdate = () => {
             'Content-Type': 'application/json',
              authorization:`${token}`
           },
-          body: JSON.stringify({ email }),
+          body: JSON.stringify({ selectedCode,email }),
          
+          
         });
         const data = await response.json()
         console.log(data)
         if (!data) {
           setLoading(true)
         }if (data.success == true) {
-        //  setSendedCode(data.confirmationCode)
           setLoading(false) 
+          setShow(true)
+          setSeconds(60)
         }if (data.success == false) {
           setMessage(data.message)
           setOpens(true)
@@ -96,9 +118,72 @@ export const EmailUpdate = () => {
       } catch (error) {
         console.error('operation failed.');
       }
-       
       }
 
+
+      
+      const handelConfirmNewEmail=async()=>{
+
+        const email=userInfo.email
+
+        try{
+        const response = await fetch(`http://localhost:8000/updateconfirmenewmail`,{
+          method:'PATCH',
+          credentials:"include", 
+          headers: {
+            'Content-Type': 'application/json',
+             authorization:`${token}`
+          },
+          body: JSON.stringify({ newEmail }),
+         
+          
+        });
+        const data = await response.json()
+        console.log(data)
+        if (!data) {
+          setLoading(true)
+        }if (data.success == true) {
+          setLoading(false) 
+          dispatch(setUserInfo(data.userInfo))
+          navigate('/settings')
+        }if (data.success == false) {
+          setMessage(data.message)
+          setOpens(true)
+        }  
+      } catch (error) {
+        console.error('operation failed.');
+      }
+      }
+
+
+      if (seconds === 0 ) {
+        const handelRemoveCode=async()=>{
+          try{
+            const response = await fetch(`http://localhost:8000/resendcode`,{
+              method:'PATCH',
+              credentials:"include", 
+              headers: {
+                'Content-Type': 'application/json',
+                 authorization:`${token}`
+              },
+            });
+            const data = await response.json()
+        
+            if (!data) {
+              setLoading(true)
+            }if (data.success == true) {
+              setLoading(false) 
+              setSeconds(60)
+            }if (data.success == false) {
+              setMessage(data.message)
+              setOpens(true)
+            }  
+          } catch (error) {
+            console.error('operation failed.');
+          }
+        }
+        handelRemoveCode()
+      }
     
 
       function maskEmail(email:any) {
@@ -116,6 +201,7 @@ export const EmailUpdate = () => {
 
       const email = maskEmail(userInfo.email);
 
+   
      
 
   return (
@@ -130,7 +216,7 @@ export const EmailUpdate = () => {
           {message}
         </Alert>
       </Snackbar>
-        <Box sx={{width:'20%',display:'flex',flexDirection:'column',alignItems:'center',p:2,my:3}} >
+        { show == false  ? <Box sx={{width:'20%',display:'flex',flexDirection:'column',alignItems:'center',p:2,my:3}} >
             <Box sx={{display:'flex',flexDirection:'column',width:'100%'}} >
             <Typography  sx={{textAlign:'left',fontWeight:'700'}}  variant='h6' gutterBottom>
             Code de vérification envoyé par e-mail
@@ -156,15 +242,25 @@ export const EmailUpdate = () => {
   onChange={(e:any)=>setSelectedCode(e.target.value)}
     />
      
-     <Button onClick={handelUpdateEmail} variant='text' sx={{color:'#2196f3',my:2,textTransform:'lowercase' ,":hover":{color:'#2196f3'} }} >
+     {  seconds === 60 ? <Button onClick={handelUpdateEmail} variant='text' sx={{color:'#2196f3',my:2,textTransform:'lowercase' ,":hover":{color:'#2196f3'} }} >
         Renvoyer un Code
     </Button>
+  :
+  
+  <Button  disabled variant='text' sx={{color:'#2196f3',my:2,textTransform:'lowercase' ,":hover":{color:'#2196f3'} }} >
+        Renvoyer un Code
+    </Button>
+  
+  }
+    { seconds < 60  ?  <Typography  sx={{textAlign:'left',mx:1,fontWeight:'400',color:'#2196f3'}}  variant='caption' gutterBottom>
+        the code available to {seconds}
+        </Typography> : "" }
    
       </Box>
 
       <LoadingButton
           color="secondary"
-          onClick={handelUpdateEmail}
+          onClick={handelConfirmEmailCode}
           loading={loading}
           loadingPosition="start"
           sx={{color:'white',width:'100%',bgcolor:'#d32f2f',borderRadius:'12px',my:2 ,":hover":{color:'white',bgcolor:'#d32f2f'} }}
@@ -174,6 +270,49 @@ export const EmailUpdate = () => {
         </LoadingButton>
 
         </Box>
+       :
+        <Box sx={{width:'20%',display:'flex',flexDirection:'column',alignItems:'center',p:2,my:3}} >
+            <Box sx={{display:'flex',flexDirection:'column',width:'100%'}} >
+            <Typography  sx={{textAlign:'left',fontWeight:'700'}}  variant='h6' gutterBottom>
+            Update New Email 
+        </Typography>
+        <Typography  sx={{textAlign:'left',fontWeight:'200'}}  variant='body1' gutterBottom>
+        Entrez your new email to update it 
+        </Typography>
+            </Box>
+      
+        <Box sx={{display:'flex',flexDirection:'column',alignItems:'flex-start',width:'100%',my:2}} >
+      
+
+   
+
+<TextField
+  id="livraison time"
+  sx={{ width: '100%' ,bgcolor:'white'}}
+  placeholder="New Email"
+  size='medium'
+  label='New Email'
+  type='email'
+  required
+  onChange={(e:any)=>setNewEmail(e.target.value)}
+    />
+     
+      </Box>
+
+      <LoadingButton
+          color="secondary"
+          onClick={handelConfirmNewEmail}
+          loading={loading}
+          loadingPosition="start"
+          sx={{color:'white',width:'100%',bgcolor:'#d32f2f',borderRadius:'12px',my:2 ,":hover":{color:'white',bgcolor:'#d32f2f'} }}
+          variant="contained"
+        >
+          <span> Update</span>
+        </LoadingButton>
+
+        </Box>}
+
+
     </div>
   )
 }
